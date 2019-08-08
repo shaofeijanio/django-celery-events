693406cca7732a97465a4a2ee8f0f750131f7431
fei <shaofei330@gmail.com>
2019-08-08 18:52:25 +0800

    Add typing to app and registry for code hint

--- django_celery_events/__init__.py
+++ django_celery_events/__init__.py
@@ -1,8 +1,11 @@
+from typing import Type
+
 from celery_events import App
+from celery_events.app import Registry
 
 from django_celery_events import configs
 
 default_app_config = 'django_celery_events.apps.DjangoCeleryEventsConfig'
 
-app = None
-registry = None
+app: Type[App] = None
+registry: Type[Registry] = None
